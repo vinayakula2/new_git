d297bd80798b8f5ed2294b5e2bc7131db9d3a83f
vinay <akulavinay134@gmail.com>
2025-07-26 10:55:49 +0530

    testing

--- git.py
+++ git.py
@@ -1,2 +1,16 @@
-print("welcome to github")
-print("welcome swetha")
+import pytest
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def test_add():
+    assert add(1 , 2) == 3
+    assert add(2,4) != 4
+    assert add(3,6) == 9
+
+def test_sub():
+    assert sub(1,3) == -2
+    assert sub(2,6) != 4
